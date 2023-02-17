f9d39a0ffb65eba225758a76c9ef1cfabafd2544
Paul Hansen <phgraphics@gmail.com>
2023-02-16 23:38:35 -0800

    update

--- assets/js/scenes/Game.js
+++ assets/js/scenes/Game.js
@@ -10,10 +10,16 @@ export default class Game extends Phaser.Scene
     preload()
     {
         this.load.image('background', '/bg_layer1.png')
+        // load the platform image
+        this.load.image('platform', '/ground_grass.png')
+        
     }
 
     create()
     {
         this.add.image(240, 320, 'background')
+
+        // add a platform image in the middle
+        this.add.image(240, 320, 'platform')
     }
 }
