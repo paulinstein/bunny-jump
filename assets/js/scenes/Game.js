dc70e92f18a0b2f4404427c18e2dd095a6e6e560
Paul Hansen <phansen@illuminateed.net>
2023-02-17 00:18:26 -0800

    changes

--- assets/js/scenes/Game.js
+++ assets/js/scenes/Game.js
@@ -10,8 +10,8 @@ export default class Game extends Phaser.Scene
     preload()
     {
         this.load.image('background', '/bg_layer1.png')
-        // load the platform image
         this.load.image('platform', '/ground_grass.png')
+        this.load.image('bunny-stand', '/bunny1_stand.png')
         
     }
 
@@ -19,7 +19,27 @@ export default class Game extends Phaser.Scene
     {
         this.add.image(240, 320, 'background')
 
-        // add a platform image in the middle
-        this.add.image(240, 320, 'platform')
+        // create the group
+        const platforms = this.physics.add.staticGroup()
+
+        // then create 5 platforms from the group
+        for (let i = 0; i < 5; ++i)
+        {
+            const x = Phaser.Math.Between(80,400)
+            const y = 150 * i
+
+            /** @type {Phaser.Physics.Arcade.Sprite} */
+            const platform = platforms.create(x, y, 'platform')
+            platform.scale = 0.5
+
+            /** @type {Phaser.Physics.Arcade.StaticBody} */
+            const body = platform.body
+            body.updateFromGameObject()
+        }
+        this.physics.add.image(240, 320, 'platform')
+        .setScale(0.5)
+
+        const player = this.physics.add.sprite(240, 320, 'bunny-stand').setScale(0.5)
+        this.physics.add.collider(platforms, player)
     }
 }
